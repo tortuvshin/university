51d663789e851bb8d1a8cd84e888a2c653a438a5
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-09-30 22:59:07 +0800

    admin_controller, my_controller updated

--- mvc/errors/error_404.php
+++ mvc/errors/error_404.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>404 Page Not Found</title>
+<title>404 Хуудас олдсонгүй</title>
 <style type="text/css">
 
 ::selection{ background-color: #E13300; color: white; }
