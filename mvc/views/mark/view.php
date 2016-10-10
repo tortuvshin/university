6f23c0272997eb976b4587d7318b106371beb84a
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-10-10 11:56:20 +0800

    Оюутан дүн харах хэсгийн дээд дүн алдаа зассан  #7

--- mvc/views/mark/view.php
+++ mvc/views/mark/view.php
@@ -133,9 +133,12 @@
                                                                 echo "<th>";
                                                                     echo $this->lang->line("mark_grade");
                                                                 echo "</th>";
-                                                                // echo "<th>";
-                                                                //     echo $this->lang->line("mark_highest_mark");
-                                                                // echo "</th>";
+
+                                                                if($usertype == "Admin" || $usertype == "Teacher" ) {
+                                                                    echo "<th>";
+                                                                        echo $this->lang->line("mark_highest_mark");
+                                                                    echo "</th>";
+                                                                }
                                                             }
                                                         echo "</tr>";
                                                     echo "</thead>";
@@ -167,9 +170,13 @@
                                                             }
                                                         }
                                                     }
-                                                    // echo "<td data-title='".$this->lang->line('mark_highest_mark')."'>";
-                                                    //     echo $mark->highestmark;
-                                                    // echo "</td>";
+
+
+                                                    if($usertype == "Admin" || $usertype == "Teacher" ) {
+                                                        echo "<td data-title='".$this->lang->line('mark_highest_mark')."'>";
+                                                            echo $mark->highestmark;
+                                                        echo "</td>";
+                                                    }
                                                 echo "</tr>";
                                             }
                                         }
