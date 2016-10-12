130138c3fa672b07e3dc4fda8534e4355c74cb6d
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-10-12 12:43:21 +0800

    gulp less ашиглах боломжтой болгосон #19

--- README.md
+++ README.md
@@ -6,8 +6,22 @@
 
 	composer install/update
 
+	npm install 
+
+	bower install
+
 #### Dompdf дээр ашиглаж байгаа сангууд суулгах
 
 	cd mvc/libraries/dompdf
 
 	composer install/update
+
+
+### Less compile хийх 
+
+	gulp less
+
+### Javascript minify хийх
+
+	gulp js
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,49 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-var path = require('path');
- 
+var gulp = require('gulp'),
+	less = require('gulp-less'),
+	path = require('path'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+	jshint = require('gulp-jshint');
+
+// gulp.task('browser-sync', function () {
+//    var files = [
+//       'app/**/*.html',
+//       'app/assets/css/**/*.css',
+//       'app/assets/imgs/**/*.png',
+//       'app/assets/js/**/*.js'
+//    ];
+
+//    browserSync.init(files, {
+//       server: {
+//          baseDir: './app'
+//       }
+//    });
+// });
+
 gulp.task('less', function () {
-  return gulp.src('./assets/**/*.less')
+  return gulp.src('./assets/inisys/main.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(gulp.dest('./public/css'));
+    .pipe(gulp.dest('./assets/inisys'));
 });
 
+// gulp.task('compile-less', function () {
+//     gulp.src('./Less/one.less') // path to your file
+//     .pipe(less())
+//     .pipe(gulp.dest('path/to/destination'));
+// });
+
+gulp.task('default', function () {
+   
+});
+
+
+gulp.task('js', function () {
+   return gulp.src('assets/inisys/js/*.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'))
+      .pipe(uglify())
+      .pipe(concat('app.js'))
+      .pipe(gulp.dest('build'));
+});
