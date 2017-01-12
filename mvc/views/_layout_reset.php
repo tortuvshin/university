feb0e05f3912b86563752317227be80cbb63285e
doljko <doljko_tv@yahoo.com>
2017-01-12 16:58:23 +0800

    reset,login footer

--- mvc/views/_layout_reset.php
+++ mvc/views/_layout_reset.php
@@ -4,9 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-   <title>Нууц үг шинэчлэх</title>
- +    <link rel="SHORTCUT ICON" href="<?=base_url("uploads/images/$siteinfos->photo")?>" />
-
+    <title>Нууц үг шинэчлэх</title>
+    <link rel="SHORTCUT ICON" href="<?=base_url("uploads/images/$siteinfos->photo")?>" />
     <!-- bootstrap 3.0.2 -->
     <link href="<?php echo base_url('assets/bootstrap/bootstrap.min.css'); ?>" rel="stylesheet"  type="text/css">
     <!-- font Awesome -->
@@ -16,46 +15,47 @@
 </head>
 
 <style type="text/css">
-   .white-bg-login {
+    .white-bg-login {
         background-image: url(../uploads/images/citi_back_login.jpg);
         background-repeat: no-repeat; 
-         background-size: cover;   
+        background-size: cover;   
     }
- </style>
+</style>
 
 <body class="white-bg-login">
-    <div class="col-md-4 col-md-offset-4 marg" style="margin-top:30px;">
+
     <div class="login-header">
-         <div class="login-control">
+        <div class="login-control">
         <?php
             if(count($siteinfos->photo)) {
-               echo "<img class='login-logo' width='50' height='50' src=".base_url('uploads/images/'.$siteinfos->photo)." />";
+                echo "<img class='login-logo' width='50' height='50' src=".base_url('uploads/images/'.$siteinfos->photo)." />";
             }
         ?>
         <h4 style="float:left;"><?php echo $siteinfos->sname; ?></h4>    
         </div>
+        
     </div>
 
     <?php $this->load->view($subview); ?>
+
 <script type="text/javascript" src="<?php echo base_url('assets/inisys/jquery.js'); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/bootstrap/bootstrap.min.js'); ?>"></script>
-
-<footer class="login-footer">
-     <span>
-         <a href="https://www.citi.edu.mn" target="_blank">Сити их сургууль © 2016 он</a>
-         <a href="https://www.facebook.com/citiuniversity" target="_blank">
-             <i class="fa fa-facebook-official" aria-hidden="true"></i>
-         </a>
-         <a href="https://twitter.com/CitiUniversity" target="_blank">
-             <i class="fa fa-twitter" aria-hidden="true"></i>
-        </a>
-         <a href="https://www.instagram.com/citiuniversity" target="_blank">
-             <i class="fa fa-instagram" aria-hidden="true"></i>
-         </a>
-         <a href="https://www.youtube.com/channel/UCROeKzkani6QRJYmmVsa92g" target="_blank">
-             <i class="fa fa-youtube-play" aria-hidden="true"></i>
-         </a>
-     </span>
- </footer>
 </body>
+<footer class="login-footer">
+    <span>
+        <a href="https://www.citi.edu.mn" target="_blank"><?php echo $siteinfos->sname; ?> © 2016 он</a>
+        <a href="https://www.facebook.com/citiuniversity" target="_blank">
+            <i class="fa fa-facebook-official" aria-hidden="true"></i>
+        </a>
+        <a href="https://twitter.com/CitiUniversity" target="_blank">
+            <i class="fa fa-twitter" aria-hidden="true"></i>
+        </a>
+        <a href="https://www.instagram.com/citiuniversity" target="_blank">
+            <i class="fa fa-instagram" aria-hidden="true"></i>
+        </a>
+        <a href="https://www.youtube.com/channel/UCROeKzkani6QRJYmmVsa92g" target="_blank">
+            <i class="fa fa-youtube-play" aria-hidden="true"></i>
+        </a>
+    </span>
+</footer>
 </html>
--- mvc/views/_layout_signin.php
+++ mvc/views/_layout_signin.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-    <title>Sign in</title>
+    <title>Нэвтрэх хэсэг</title>
     <link rel="SHORTCUT ICON" href="<?=base_url("uploads/images/$siteinfos->photo")?>" />
     <!-- bootstrap 3.0.2 -->
     <link href="<?php echo base_url('assets/bootstrap/bootstrap.min.css'); ?>" rel="stylesheet"  type="text/css">
@@ -14,46 +14,37 @@
     <link href="<?php echo base_url('assets/inisys/style.css'); ?>" rel="stylesheet"  type="text/css">
 </head>
 
+<style type="text/css">
+    .white-bg-login {
+        background-image: url(../uploads/images/citi_back_login.jpg);
+        background-repeat: no-repeat; 
+        background-size: cover;   
+    }
+</style>
+
 <body class="white-bg-login">
 
-    <div class="col-md-4 col-md-offset-4 marg" style="margin-top:30px;">
+    <div class="login-header">
+        <div class="login-control">
         <?php
             if(count($siteinfos->photo)) {
-                echo "<center><img width='50' height='50' src=".base_url('uploads/images/'.$siteinfos->photo)." /></center>";
+                echo "<img class='login-logo' width='50' height='50' src=".base_url('uploads/images/'.$siteinfos->photo)." />";
             }
         ?>
-        <center><h4><?php echo $siteinfos->sname; ?></h4></center>
+        <h4 style="float:left;"><?php echo $siteinfos->sname; ?></h4>    
+        </div>
+        
     </div>
 
     <?php $this->load->view($subview); ?>
 
-    
-
 <script type="text/javascript" src="<?php echo base_url('assets/inisys/jquery.js'); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/bootstrap/bootstrap.min.js'); ?>"></script>
- <style type="text/css">
-         .white-bg-login {
-         background-image: url(../uploads/images/citi_back_login.jpg);
-        background-repeat: no-repeat; 
-        background-size: cover;   
-    }
-
- 
- <div class="login-header">
-      <div class="login-control">
-          <?php
-              if(count($siteinfos->photo)) {
-                  echo "<img class='login-logo' width='50' height='50' src=".base_url('uploads/images/'.$siteinfos->photo)." />";
-              }
-          ?>
-       <h4 style="float:left;"><?php echo $siteinfos->sname; ?></h4>    
-    </div>        
-</div>
-
+</body>
 <footer class="login-footer">
     <span>
-        <a href="https://www.citi.edu.mn" target="_blank">Сити их сургууль © 2016 он</a>
-         <a href="https://www.facebook.com/citiuniversity" target="_blank">">
+        <a href="https://www.citi.edu.mn" target="_blank"><?php echo $siteinfos->sname; ?> © 2016 он</a>
+        <a href="https://www.facebook.com/citiuniversity" target="_blank">
             <i class="fa fa-facebook-official" aria-hidden="true"></i>
         </a>
         <a href="https://twitter.com/CitiUniversity" target="_blank">
@@ -62,12 +53,9 @@
         <a href="https://www.instagram.com/citiuniversity" target="_blank">
             <i class="fa fa-instagram" aria-hidden="true"></i>
         </a>
-       <a href="https://www.youtube.com/channel/UCROeKzkani6QRJYmmVsa92g" target="_blank">
+        <a href="https://www.youtube.com/channel/UCROeKzkani6QRJYmmVsa92g" target="_blank">
             <i class="fa fa-youtube-play" aria-hidden="true"></i>
         </a>
     </span>
 </footer>
-
-
-</body>
 </html>
