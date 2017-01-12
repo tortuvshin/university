950c832a56386be1c29d512843907e6ee62253ea
doljko <doljko_tv@yahoo.com>
2017-01-12 17:01:16 +0800

    Social holboos # bolgov..

--- mvc/views/_layout_signin.php
+++ mvc/views/_layout_signin.php
@@ -43,17 +43,17 @@
 </body>
 <footer class="login-footer">
     <span>
-        <a href="https://www.citi.edu.mn" target="_blank"><?php echo $siteinfos->sname; ?> © 2016 он</a>
-        <a href="https://www.facebook.com/citiuniversity" target="_blank">
+        <a href="#"  target="_blank"><?php echo $siteinfos->sname; ?> © 2016 он</a>
+        <a href="#"  target="_blank">
             <i class="fa fa-facebook-official" aria-hidden="true"></i>
         </a>
-        <a href="https://twitter.com/CitiUniversity" target="_blank">
+        <a href="#" target="_blank">
             <i class="fa fa-twitter" aria-hidden="true"></i>
         </a>
-        <a href="https://www.instagram.com/citiuniversity" target="_blank">
+        <a href="#"  target="_blank">
             <i class="fa fa-instagram" aria-hidden="true"></i>
         </a>
-        <a href="https://www.youtube.com/channel/UCROeKzkani6QRJYmmVsa92g" target="_blank">
+        <a href="#"  target="_blank">
             <i class="fa fa-youtube-play" aria-hidden="true"></i>
         </a>
     </span>
