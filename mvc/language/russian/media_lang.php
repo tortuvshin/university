044980a17773fa2cd030c0a676504b0fc152a694
Mars-Pc\Mars <info@tagtaasolution.mn>
2017-01-12 18:29:29 +0800

    Media language updated

---
+++ mvc/language/russian/media_lang.php
@@ -0,0 +1,28 @@
+<?php
+
+/* List Language  */
+$lang['panel_title'] = "Медиа";
+$lang['add_title'] = "Хавтас үүсгэх";
+$lang['slno'] = "#";
+$lang['media_title'] = "Гарчиг";
+$lang['media_date'] = "Огноо";
+$lang['action'] = "Үйлдэл";
+
+$lang['view'] = 'Харах';
+$lang['edit'] = 'Засах';
+$lang['delete'] = 'Устгах';
+
+/* Add Language */
+
+$lang['add_class'] = 'Медиа нэмэх';
+$lang['update_class'] = 'Медиа шинэчлэх';
+$lang['file'] = 'Медиа';
+$lang['upload_file'] = 'Медиа хуулах';
+$lang['folder_name'] = 'Хавтасны нэр';
+$lang['share'] = 'Хуваалцах';
+$lang['share_with'] = 'Share with';
+$lang['select_class'] = 'Курс сонгох';
+$lang['all_class'] = 'Бүх курс';
+$lang['public'] = 'Нийтийн';
+$lang['class'] = 'Курс';
+
